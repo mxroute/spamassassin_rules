8b17f776f8f4f8ade8f636931d14d3c1d7c35c42
mxroute <37432698+mxroute@users.noreply.github.com>
2021-05-18 22:52:54 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,51 +3,51 @@
 # Build the top
 # https://github.com/mxroute/spamassassin_rules
 
-rm -f local.cf
-curl https://raw.githubusercontent.com/mxroute/spamassassin_rules/main/header >> local.cf
-echo "" >> local.cf
-curl https://raw.githubusercontent.com/mxroute/spamassassin_rules/main/trusted_networks >> local.cf
-echo "" >> local.cf
-curl https://raw.githubusercontent.com/mxroute/spamassassin_rules/main/blacklist_from >> local.cf
-echo "" >> local.cf
-curl https://raw.githubusercontent.com/mxroute/spamassassin_rules/main/whitelist_from >> local.cf
-echo "" >> local.cf
-curl https://raw.githubusercontent.com/mxroute/spamassassin_rules/main/score >> local.cf
+rm -f /var/www/config/spam/local.cf
+curl https://raw.githubusercontent.com/mxroute/spamassassin_rules/main/header >> /var/www/config/spam/local.cf
+echo "" >> /var/www/config/spam/local.cf
+curl https://raw.githubusercontent.com/mxroute/spamassassin_rules/main/trusted_networks >> /var/www/config/spam/local.cf
+echo "" >> /var/www/config/spam/local.cf
+curl https://raw.githubusercontent.com/mxroute/spamassassin_rules/main/blacklist_from >> /var/www/config/spam/local.cf
+echo "" >> /var/www/config/spam/local.cf
+curl https://raw.githubusercontent.com/mxroute/spamassassin_rules/main/whitelist_from >> /var/www/config/spam/local.cf
+echo "" >> /var/www/config/spam/local.cf
+curl https://raw.githubusercontent.com/mxroute/spamassassin_rules/main/score >> /var/www/config/spam/local.cf
 
 # Blacklisted Senders
 # https://github.com/mxroute/rspamd_rules/blob/master/lists/blacklisted-sender-strings.map
 
-echo -n "\nheader SPAMDOMAINS From =~ /" >> local.cf &&
+echo -n "\nheader SPAMDOMAINS From =~ /" >> /var/www/config/spam/local.cf &&
 curl https://raw.githubusercontent.com/mxroute/rspamd_rules/master/lists/blacklisted-sender-strings.map | sed 's/\///' | sed 's/\/i//' | sed 's/(//' | sed 's/)//' |
 while IFS= read -r line
 do
 	echo -n "$line|"
-done | rev | cut -c 2- | rev | sed '1s/^/(/' | sed 's/$/)\/i/' >> local.cf
-echo "describe SPAMDOMAINS Spammers" >> local.cf
-echo "score SPAMDOMAINS 10" >> local.cf
+done | rev | cut -c 2- | rev | sed '1s/^/(/' | sed 's/$/)\/i/' >> /var/www/config/spam/local.cf
+echo "describe SPAMDOMAINS Spammers" >> /var/www/config/spam/local.cf
+echo "score SPAMDOMAINS 10" >> /var/www/config/spam/local.cf
 
 # Body Spam
 # https://github.com/mxroute/rspamd_rules/blob/master/lists/body-spam.map
 
-echo -n "\nheader GENSPAMBODY From =~ /" >> local.cf &&
+echo -n "\nheader GENSPAMBODY /" >> /var/www/config/spam/local.cf &&
 curl https://raw.githubusercontent.com/mxroute/rspamd_rules/master/lists/body-spam.map | sed 's/\///' | sed 's/\/i//' | sed 's/(//' | sed 's/)//' |
 while IFS= read -r line
 do
 	echo -n "$line|"
-done | rev | cut -c 2- | rev | sed '1s/^/(/' | sed 's/$/)\/i/' >> local.cf
-echo "describe GENSPAMBODY Email contained known spam string" >> local.cf
-echo "score GENSPAMBODY 10" >> local.cf
+done | rev | cut -c 2- | rev | sed '1s/^/(/' | sed 's/$/)\/i/' >> /var/www/config/spam/local.cf
+echo "describe GENSPAMBODY Email contained known spam string" >> /var/www/config/spam/local.cf
+echo "score GENSPAMBODY 10" >> /var/www/config/spam/local.cf
 
 # Subject Spam
 # https://github.com/mxroute/rspamd_rules/blob/master/lists/subject-spam.map
 
-echo -n "\nheader GENSPAMSUBJ Subject =~ /" >> local.cf &&
+echo -n "\nheader GENSPAMSUBJ Subject =~ /" >> /var/www/config/spam/local.cf &&
 curl https://raw.githubusercontent.com/mxroute/rspamd_rules/master/lists/subject-spam.map | sed 's/\///' | sed 's/\/i//' | sed 's/(//' | sed 's/)//' |
 while IFS= read -r line
 do
 	echo -n "$line|"
-done | rev | cut -c 2- | rev | sed '1s/^/(/' | sed 's/$/)\/i/' >> local.cf
-echo "describe GENSPAMSUBJ Subject contained known spam string" >> local.cf
-echo "score GENSPAMSUBJ 10" >> local.cf
-echo "" >> local.cf
-echo "# Last edited $(date)" >> local.cf
+done | rev | cut -c 2- | rev | sed '1s/^/(/' | sed 's/$/)\/i/' >> /var/www/config/spam/local.cf
+echo "describe GENSPAMSUBJ Subject contained known spam string" >> /var/www/config/spam/local.cf
+echo "score GENSPAMSUBJ 10" >> /var/www/config/spam/local.cf
+echo "" >> /var/www/config/spam/local.cf
+echo "# Last edited $(date)" >> /var/www/config/spam/local.cf
