c59552a0f32155da2c6c0c76ff18e13f3da213fa
mxroute <37432698+mxroute@users.noreply.github.com>
2023-03-14 15:32:19 -0500

    Update update_sa_rules.sh

--- update_sa_rules.sh
+++ update_sa_rules.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://config.mxroute.com/spam/local.cf -O /etc/mail/spamassassin/local.cf
+wget https://raw.githubusercontent.com/mxroute/spamassassin_rules/main/local.cf -O /etc/mail/spamassassin/local.cf
 /scripts/restartsrv_spamd
